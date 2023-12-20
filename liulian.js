227ade4f7c26c59b3b6187b266212d7049f20a99
ZR19050005-shajinge <shajinge@zhongruigroup.com>
2023-12-20 14:45:54 +0800

    点击切换场景’

--- liulian.js
+++ liulian.js
@@ -1,9 +1,11 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import TWEEN from "@tweenjs/tween.js";
 var scene,
   camera,
   renderer,
-  poiObjects = [];
+  poiObjects = [],
+  countI = 0;
 
 var width = window.innerWidth;
 var height = window.innerHeight;
@@ -12,7 +14,7 @@ function initThree() {
   //场景
   scene = new THREE.Scene();
   //镜头
-  camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 100);
+  camera = new THREE.PerspectiveCamera(90, width / height, 0.01, 100);
   camera.position.set(0, 0, 0.01);
   //渲染器
   renderer = new THREE.WebGLRenderer();
@@ -20,26 +22,59 @@ function initThree() {
   document.getElementById("webgl").appendChild(renderer.domElement);
   //镜头控制器
   var controls = new OrbitControls(camera, renderer.domElement);
+  // controls.enableZoom = false; //禁止缩放
+  setBox();
 
+  render();
+}
+function setBox() {
   var materials = [];
+  var texture_right,
+    texture_left,
+    texture_top,
+    texture_bottom,
+    texture_front,
+    texture_back;
   //根据右左上下前后的顺序构建六个面的材质集
-  var texture_right = new THREE.TextureLoader().load("./images/r.jpg");
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_right }));
+  if (countI % 2 === 0) {
+    texture_right = new THREE.TextureLoader().load("./images/r.jpg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_right }));
 
-  var texture_left = new THREE.TextureLoader().load("./images/l.jpg");
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_left }));
+    texture_left = new THREE.TextureLoader().load("./images/l.jpg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_left }));
 
-  var texture_top = new THREE.TextureLoader().load("./images/u.jpg");
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_top }));
+    texture_top = new THREE.TextureLoader().load("./images/u.jpg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_top }));
 
-  var texture_bottom = new THREE.TextureLoader().load("./images/d.jpg");
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_bottom }));
+    texture_bottom = new THREE.TextureLoader().load("./images/d.jpg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_bottom }));
 
-  var texture_front = new THREE.TextureLoader().load("./images/f.jpg");
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_front }));
+    texture_front = new THREE.TextureLoader().load("./images/f.jpg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_front }));
 
-  var texture_back = new THREE.TextureLoader().load("./images/b.jpg");
-  materials.push(new THREE.MeshBasicMaterial({ map: texture_back }));
+    texture_back = new THREE.TextureLoader().load("./images/b.jpg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_back }));
+  } else {
+    texture_left = new THREE.TextureLoader().load("./images/scene_left.jpeg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_left }));
+
+    texture_right = new THREE.TextureLoader().load("./images/scene_right.jpeg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_right }));
+
+    texture_top = new THREE.TextureLoader().load("./images/scene_top.jpeg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_top }));
+
+    texture_bottom = new THREE.TextureLoader().load(
+      "./images/scene_bottom.jpeg"
+    );
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_bottom }));
+
+    texture_front = new THREE.TextureLoader().load("./images/scene_front.jpeg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_front }));
+
+    texture_back = new THREE.TextureLoader().load("./images/scene_back.jpeg");
+    materials.push(new THREE.MeshBasicMaterial({ map: texture_back }));
+  }
 
   var box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), materials);
   scene.add(box);
@@ -50,7 +85,7 @@ function initThree() {
     {
       position: {
         x: 0,
-        y: 0,
+        y: 0.2,
         z: -0.2,
       },
       detail: {
@@ -85,8 +120,6 @@ function initThree() {
 
     scene.add(sprite);
   }
-
-  loop();
 }
 document.querySelector("#webgl").addEventListener("click", function (event) {
   event.preventDefault();
@@ -101,13 +134,32 @@ document.querySelector("#webgl").addEventListener("click", function (event) {
 
   var intersects = raycaster.intersectObjects(poiObjects);
   if (intersects.length > 0) {
-    alert("点击了热点" + intersects[0].object.detail.title);
+    countI++;
+    var clickedFruit = intersects[0];
+    console.log(clickedFruit);
+    new TWEEN.Tween(camera.position)
+      .to(
+        {
+          x: clickedFruit.object.position.x,
+          y: clickedFruit.object.position.y,
+          z: clickedFruit.object.position.z + 0.02,
+        },
+        2000
+      )
+      .start()
+      .easing(TWEEN.Easing.Quadratic.Out) //使用二次缓动函数
+      .onComplete(function () {
+        setBox();
+        camera.position.set(0, 0, 0.01);
+        camera.lookAt(0, 0, 0);
+      });
   }
 });
 
 //帧同步重绘
-function loop() {
-  requestAnimationFrame(loop);
+function render() {
+  TWEEN.update();
+  requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
 
